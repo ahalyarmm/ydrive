da1079317cbbbfb587352f61a4141d0cd3d0b3fe
Ahalya <ahalyarmm>
2016-09-03 10:47:45 +1000

    Added google maps intent

--- app/src/main/java/com/example/ahalya/frontscreen/FindOrMakeAWalkingGroup.java
+++ app/src/main/java/com/example/ahalya/frontscreen/FindOrMakeAWalkingGroup.java
@@ -1,6 +1,8 @@
 package com.example.ahalya.frontscreen;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,9 +19,16 @@ public class FindOrMakeAWalkingGroup extends AppCompatActivity {
         setContentView(R.layout.activity_find_or_make_awalking_group);
     }
 
-    public void OnButton8Click(View v)
+    public void OnListOfWalkingGroupsButtonClick(View v)
     {
         Intent intent = new Intent(FindOrMakeAWalkingGroup.this, ListOfWalkingGroups.class);
+        startActivity(intent);
+    }
+
+    public void OnFindYourWayToSchoolButtonClick(View v)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?f=d&daddr=-27.499769,152.966786"));
+        intent.setComponent(new ComponentName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity"));
         startActivity(intent);
     }
 }
